2e18b9580e1a0f03d993d738d2bf588dcfc8da9e
= <kennylalek@gmail.com>
2024-08-05 00:50:38 +0100

    routes and functions for comparison tests

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, request, jsonify
 from utility import *
+import numpy as np
 
 app = Flask(__name__)
 
@@ -15,10 +16,13 @@ def tests() :
 def conformity_tests() :
     return render_template('conformity-tests.html')
 
+@app.route('/comparison-tests')
+def comparison_tests() :
+    return render_template('comparison-tests.html')
+
 
 @app.route('/get-result', methods = ['POST'])
 def get_result() :
-
     text = None
     if request.method == 'POST' :
         try :
@@ -44,7 +48,6 @@ def get_result() :
             
             elif parameter == 'Proportion' :
                 proportion = float(request.form['sample-mean'])
-                print('done here !!!')
                 result = proportion_comformity_test(proportion, test_value, n, alpha / 100, hypothesis)
             
 
@@ -69,14 +72,98 @@ def get_result() :
                 "text" : text
             }
 
-            print(data)
-
         except Exception as e :
             return e
         
     return jsonify(data)
 
-       
+
+@app.route('/get-table-data', methods = ['POST'])
+def table() :
+    data = request.json.get('data')
+    print(data)
+
+    if data:
+        data = [float(x) for x in data]
+        mean_value = np.mean(data)
+        print(mean_value)
+        std_value = np.std(data)
+    else:
+        mean_value = None
+        std_value = None
+
+    result = {
+        'success' : True,
+        'mean' : mean_value,
+        'std' : std_value
+    }
+
+    return jsonify(result)
+
+
+
+@app.route('/get-params', methods = ['POST'])
+def get_params() :
+    if request.method == 'POST' :
+        try :
+            parameter = request.form['parameter']
+            n_1 = int(request.form['sample-size-1'])
+            n_2 = int(request.form['sample-size-2'])
+            test_type = int(request.form['hypothesis'])
+            alpha = int(request.form['alpha'])
+            
+            if parameter == 'Mean' :
+                mean_1 = float(request.form['sample-mean-1'])
+                mean_2 = float(request.form['sample-mean-2'])
+
+                std_1 = float(request.form['std-1'])
+                std_2 = float(request.form['std-2'])
+
+                known = request.form['known']
+
+                if known == 'yes' :
+                    result = mean_comparison_Z_test(n_1, n_2, test_type, alpha / 100, mean_1, mean_2, std_1, std_2)
+                else :
+                    result = mean_comparison_T_test(n_1, n_2, test_type, alpha / 100, mean_1, mean_2, std_1, std_2)
+                
+                print('done here')
+
+            elif parameter == 'Variance' :
+                std_1 = float(request.form['std-1'])
+                std_2 = float(request.form['std-2'])
+
+                result = variance_comparison_test(n_1, n_2, std_1, std_2, test_type, alpha / 100)
+
+            elif parameter == 'Proportion' :
+                mean_1 = float(request.form['sample-mean-1'])
+                mean_2 = float(request.form['sample-mean-2'])
+
+                result = proportion_comparison_test(mean_1, mean_2, n_1, n_2, test_type, alpha / 100)
+
+            if result[0] :
+                text = f'we\;can\;not\;reject\;H_0'
+                desc = f"Since\;{round(result[2], 3)} \\in " + result[5] + ',\;then\;'
+            else :
+                text = 'we\;reject\;H_0'
+                desc = f"Since\;{round(result[2], 3)} \\notin " + result[5] + ',\;then\;'
+            
+            data = {
+                "parameter" : parameter,
+                "test_type" : result[1],
+                "alpha" : alpha / 100,
+                "text" : text,
+                "formula" : result[3],
+                "stat_value" : round(result[2], 3),
+                "critical_region" : result[5],
+                "symbol" : result[6],
+                "desc" : desc,
+                "text" : text
+            }
+
+        except Exception as e :
+            print(e)
+    return jsonify(data)
+
 
 if __name__ == "__main__" :
     app.run(debug = True, port=5001)
--- utility.py
+++ utility.py
@@ -1,4 +1,3 @@
-import numpy as np
 import scipy.stats as stats
 import math
 
@@ -6,7 +5,7 @@ def mean_conformity_Z_test(sample_mean, test_value, standard_deviation, sample_s
     Statistic = (sample_mean - test_value) / (standard_deviation / math.sqrt(sample_size))
 
     formula = r"\;Z = \frac{\bar{X} - \mu}{\sigma / \sqrt{n}}"
-    
+  
     if test_type == 0 :
         test_type = 'Two-tailed'
         critical_value = round(stats.norm.ppf(1 - alpha / 2, loc = 0, scale = 1), 3)
@@ -104,6 +103,130 @@ def variance_comformity_test(standard_deviation, test_value, sample_size, alpha,
     else :
         critical_value = round(stats.chi2.ppf(1 - alpha, sample_size - 1), 3)
         symbol = f"\;\chi^2_{{{alpha}}} = {critical_value}"
+
+        if test_type == 1 :
+            critical_region = f"\;[0, {critical_value}]"
+            test_type = 'Left-tailed'
+            acceptance = Statistic <= critical_value and Statistic >= 0
+        elif test_type == 2 :
+            critical_region = f"\;[{critical_value}, +\\infty["
+            test_type = 'Right-tailed'
+            acceptance = Statistic >= critical_value
+
+    return acceptance, test_type, Statistic, formula, critical_value, critical_region, symbol
+
+
+def mean_comparison_Z_test(n_1, n_2, test_type, alpha, mean_1, mean_2, std_1, std_2) :
+
+    Statistic = (mean_1 - mean_2) / math.sqrt(std_1 ** 2 / n_1 + std_2 ** 2 / n_2)
+
+    formula = r"\;Z = \frac{\bar{X_1} - \bar{X_2}}{\sqrt{\frac{\sigma^2_1}{n_1} + \frac{\sigma^2_2}{n_2}}}"
+
+    if test_type == 0 :
+        test_type = 'Two-tailed'
+        critical_value = round(stats.norm.ppf(1 - alpha / 2, loc = 0, scale = 1), 3)
+
+        symbol = f"\;z_{{{alpha / 2}}} = {critical_value}"
+
+        critical_region = f"\;[{-critical_value}, {critical_value}]"
+
+        acceptance =  Statistic <= critical_value and Statistic >= -critical_value
+    else :
+        critical_value = round(stats.norm.ppf(1 - alpha, loc = 0, scale = 1), 3)
+        symbol = f"\;z_{{{alpha}}} = {critical_value}"
+
+        if test_type == 1 :
+            critical_region = f"\;]-\\infty, {critical_value}]"
+            test_type = 'Left-tailed'
+            acceptance = Statistic <= critical_value
+        elif test_type == 2 :
+            critical_region = f"\;[{-critical_value}, +\\infty["
+            test_type = 'Right-tailed'
+            acceptance = Statistic >= -critical_value
+
+    return acceptance, test_type, Statistic, formula, critical_value, critical_region, symbol
+
+
+def mean_comparison_T_test(n_1, n_2, test_type, alpha, mean_1, mean_2, std_1, std_2) :
+
+    Statistic = (mean_1 - mean_2) / math.sqrt((1 / n_1 + 1/ n_2) * ((n_1 * std_1 ** 2 + n_2 * std_2 ** 2) / (n_1 + n_2 - 2)))
+
+    formula = r"\;T = \frac{\bar{X_1} - \bar{X_2}}{\sqrt{(\frac{1}{n_1} + \frac{1}{n_2})(\frac{n_1 S_1^2 + n_2 S_2^2}{n_1 + n_2 - 2})}}"
+
+    if test_type == 0 :
+        test_type = 'Two-tailed'
+        critical_value = round(stats.t.ppf(1 - alpha / 2, n_1 + n_2 - 2), 3)
+
+        symbol = f"\;t_{{{alpha / 2}}} = {critical_value}"
+
+        critical_region = f"\;[{-critical_value}, {critical_value}]"
+
+        acceptance =  Statistic <= critical_value and Statistic >= -critical_value
+    else :
+        critical_value = round(stats.t.ppf(1 - alpha, n_1 + n_2 - 2), 3)
+        symbol = f"\;t_{{{alpha}}} = {critical_value}"
+
+        if test_type == 1 :
+            critical_region = f"\;]-\\infty, {critical_value}]"
+            test_type = 'Left-tailed'
+            acceptance = Statistic <= critical_value
+        elif test_type == 2 :
+            critical_region = f"\;[{-critical_value}, +\\infty["
+            test_type = 'Right-tailed'
+            acceptance = Statistic >= -critical_value
+
+    return acceptance, test_type, Statistic, formula, critical_value, critical_region, symbol
+
+
+def proportion_comparison_test(p_1, p_2, n_1, n_2, test_type, alpha) :
+
+    p = (n_1 * p_1 + n_2 * p_2) / (n_1 + n_2)
+    Statistic = (p_1 - p_2) / math.sqrt(p * (1 - p) * (1 / n_1 + 1 / n_2))
+
+    formula = r'\;Z = \frac{f_1 - f_2}{\sqrt{\hat{p}(1 - \hat{p})(\frac{1}{n_1} + \frac{1}{n_2})}}'
+
+    if test_type == 0 :
+        test_type = 'Two-tailed'
+        critical_value = round(stats.norm.ppf(1 - alpha / 2, loc = 0, scale = 1), 3)
+        symbol = f"\;z_{{{alpha / 2}}} = {critical_value}"
+        critical_region = f"\;[{-critical_value}, {critical_value}]"
+        acceptance = Statistic <= critical_value and Statistic >= -critical_value
+    else :
+        critical_value = round(stats.norm.ppf(1 - alpha, loc = 0, scale = 1), 3)
+        symbol = f"\;z_{{{alpha}}} = {critical_value}"
+
+        if test_type == 1 :
+            test_type = 'Left-tailed'
+            critical_region = f"\;]-\\infty, {critical_value}]"
+            acceptance = Statistic <= critical_value
+        elif test_type == 2 :
+            critical_region = f"\;[{-critical_value}, +\\infty["
+            test_type = 'Right-tailed'
+            acceptance = Statistic >= -critical_value
+
+    return acceptance, test_type, Statistic, formula, critical_value, critical_region, symbol
+
+def variance_comparison_test(n_1, n_2, std_1, std_2, test_type, alpha) :
+    Statistic = ((n_2 - 1) / (n_1 - 1)) * ((n_1 * std_1) / (n_2 * std_2))
+
+    formula = r"\;F = \frac{n_2 - 1}{n_1 - 1} \frac{n_1 S_1^2}{n_2 S_2^2}"
+
+    if test_type == 0 :
+        test_type = 'Two-tailed'
+        critical_value_1 = round(stats.f.ppf(1 - alpha / 2, dfn = n_1 - 1, dfd = n_2 - 1), 3)
+        critical_value_2 = round(stats.f.ppf(alpha / 2, dfn = n_1 - 1, dfd = n_2 - 1), 3)
+
+        critical_value = (critical_value_1, critical_value_2)
+
+        symbol = f"\;f_{{{alpha}}} = {critical_value_1},f^2_{{{1 - alpha}}} = {critical_value_2}"
+
+        critical_region = f"\;[{critical_value_1}, {critical_value_2}]"
+
+        acceptance =  Statistic <= critical_value_2 and Statistic >= critical_value_1
+    
+    else :
+        critical_value = round(stats.f.ppf(1 - alpha, dfn = n_1 - 1, dfd = n_2 - 1), 3)
+        symbol = f"\;f_{{{alpha}}} = {critical_value}"
 
         if test_type == 1 :
             critical_region = f"\;[0, {critical_value}]"
