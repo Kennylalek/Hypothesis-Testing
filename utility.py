3c9c459d51ab16edcf450617ab29e8ba5fbebddc
= <kennylalek@gmail.com>
2024-08-07 00:53:14 +0100

    functions for independence tests

--- utility.py
+++ utility.py
@@ -1,5 +1,16 @@
+import numpy as np
 import scipy.stats as stats
 import math
+from spearman import spearman_critical_values
+
+def pearson_critical_value(alpha, df):
+    # Calculate the critical value for the t-distribution
+    t_critical = stats.t.ppf(1 - alpha / 2, df)
+    
+    # Convert t-value to Pearson correlation coefficient
+    r_critical = np.sqrt(t_critical ** 2 / (t_critical ** 2 + df))
+    
+    return r_critical
 
 def mean_conformity_Z_test(sample_mean, test_value, standard_deviation, sample_size, alpha, test_type) :
     Statistic = (sample_mean - test_value) / (standard_deviation / math.sqrt(sample_size))
@@ -206,6 +217,7 @@ def proportion_comparison_test(p_1, p_2, n_1, n_2, test_type, alpha) :
 
     return acceptance, test_type, Statistic, formula, critical_value, critical_region, symbol
 
+
 def variance_comparison_test(n_1, n_2, std_1, std_2, test_type, alpha) :
     Statistic = ((n_2 - 1) / (n_1 - 1)) * ((n_1 * std_1) / (n_2 * std_2))
 
@@ -238,3 +250,90 @@ def variance_comparison_test(n_1, n_2, std_1, std_2, test_type, alpha) :
             acceptance = Statistic >= critical_value
 
     return acceptance, test_type, Statistic, formula, critical_value, critical_region, symbol
+
+
+def qualitative_variables_test(original_array, alpha, rows, cols) :
+    array = np.array(original_array)
+
+    row_sums = np.sum(array, axis = 1)
+    col_sums = np.sum(array, axis = 0)
+    total_sum = np.sum(array)
+
+    C_array = np.outer(row_sums, col_sums) / total_sum
+
+    final = (array - C_array) ** 2 / C_array
+
+    statistic = np.sum(final)
+
+    formula = r'\;\chi_c^2 = \sum_{i, j} \frac{(n_{ij} - C_{ij})^2}{C_{ij}}'
+
+    critical_value = round(stats.chi2.ppf(1 - alpha, (rows - 1) * (cols - 1)), 3)
+
+    critical_region = f"\;[0, {critical_value}]"
+    
+    symbol = f"\;\chi^2_{{{alpha}}} = {critical_value}"
+
+    acceptance = statistic < critical_value
+
+    return acceptance, formula, symbol, critical_value, critical_region, statistic
+
+
+def null_correlation_test(original_array, alpha, sample_size) :
+    array = np.array(original_array)
+    arr_1 = np.array(original_array[0])
+    arr_2 = np.array(original_array[1])
+
+    cov_matrix = np.cov(array)
+    covariance = cov_matrix[0, 1]
+
+    print(covariance)
+    std_1 = np.std(arr_1)
+    std_2 = np.std(arr_2)
+
+    r = covariance / (std_1 * std_2)
+    print("r = ", r)
+
+    statistic = (r * math.sqrt(sample_size - 2)) / math.sqrt(1 - r ** 2)
+
+    formula = r"\;T = \frac{R \sqrt{n \;- \;2}}{\sqrt{1\; - \;R^2}}"
+
+    critical_value = round(stats.t.ppf(1 - alpha / 2,sample_size - 2), 3)
+        
+    symbol = f"\;t_{{{alpha / 2}}} = {critical_value}"
+        
+    critical_region = f"\;[{-critical_value}, {critical_value}]"
+    
+    acceptance = statistic <= critical_value and statistic >= -critical_value
+
+    return acceptance, formula, symbol, critical_value, critical_region, statistic
+ 
+
+def spearman_test(original_array, alpha, sample_size) :
+    array = np.array(original_array)
+    arr_1 = np.array(original_array[0])
+    arr_2 = np.array(original_array[1])
+
+    ranks_1 = stats.rankdata(arr_1, method='average')
+    ranks_2 = stats.rankdata(arr_2, method='average')
+
+    final = (ranks_1 - ranks_2) ** 2
+
+    statistic = 1 - (6 * np.sum(final)) / (sample_size * (sample_size ** 2 - 1))
+
+    formula = r"\;r_s = 1 - \frac{6 \sum (x^{'} - y^{'})^2}{n(n^2 - 1)}"
+
+    if sample_size <= 13 :
+        critical_value = spearman_critical_values[alpha][sample_size]
+
+    else :
+        critical_value = pearson_critical_value(alpha, sample_size - 2)
+
+    critical_region = f"\;[0, {critical_value}["
+    symbol = f"\;r_{{{alpha}}} = {critical_value}"
+
+    acceptance = abs(statistic) <= critical_value
+
+    return acceptance, formula, symbol, critical_value, critical_region, statistic
+
+data = [[1.0, 2.0, 3.0, 4.0, 5.0], [6.0, 7.0, 8.0, 9.0, 0.0]]
+print(null_correlation_test(data, 0.05, 5))
